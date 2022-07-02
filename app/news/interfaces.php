598eaa0e0eb4fa186cd5600ab53041002c79fa9f
codedk47 <zeroneta47@icloud.com>
2022-07-02 12:51:56 +0700

    1

--- app/news/interfaces.php
+++ app/news/interfaces.php
@@ -321,7 +321,7 @@ class interfaces extends webapp
 	}
 	function get_ads()
 	{
-		foreach ($this->mysql->ads('WHERE site=?i AND seat', $this->site) as $ad)
+		foreach ($this->mysql->ads('WHERE site=?i', $this->site) as $ad)
 		{
 			$this->ad_xml($ad);
 		}
