98615849afe5522d8032a7cfd8cd063553b68af6
codedk47 <zeroneta47@icloud.com>
2022-10-10 11:10:16 +0700

    1

--- app/news/admin.php
+++ app/news/admin.php
@@ -33,8 +33,8 @@ class webapp_router_admin extends webapp_echo_html
 			['Ads', '?admin/ads'],
 			['Extensions', [
 				['Reports（通告、报告，合并通用）', '?admin/reports'],
-				['Web Socket Chat System（在线客服）', '?admin/wschat'],
-				['Comments（评论记录，暂时没用）', '?admin/comments'],
+				//['Web Socket Chat System（在线客服）', '?admin/wschat'],
+				//['Comments（评论记录，暂时没用）', '?admin/comments'],
 				['Set Tags（设置首页标签，包含哪些点播）', '?admin/settags'],
 				['Set Vods（设置点播集合，类型资源）', '?admin/setvods'],
 				['Stat Bills（统计账单，包括资源购买）', '?admin/statbills'],
