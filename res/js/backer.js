3c920fc5ee9675265d516f57d482f6c9316ecd46
codedk47 <zeroneta47@icloud.com>
2022-10-10 11:09:02 +0700

    1

--- res/js/backer.js
+++ res/js/backer.js
@@ -37,60 +37,58 @@ else
 	const controller = new AbortController, queue = [], limit = 4;
 	async function load(url, type, options)
 	{
+		//application/octet-stream
+		//application/octet-masker
 		const
-		response = await fetch(url, {...options, signal: controller.signal}),
-		
-		reader = response.body.getReader(),
-		key = new Uint8Array(8),
-		buffer = [];
-
-
-		//console.log(response);
-
-		for (let read, len = 0, offset = 0;;)
+		option = {...options, signal: controller.signal}, response = await fetch(url, option), type = response.headers.get('content-type');
+		if (option.mask ||  === 'application/octet-masker')
 		{
-			read = await reader.read();
-			if (read.done)
+			let blob;
+			const reader = response.body.getReader(), key = new Uint8Array(8), buffer = [];
+			for (let read, len = 0, offset = 0;;)
 			{
-				break;
-			}
-			if (/^text/i.test(response.headers.get('content-type')))
-			{
-				buffer[buffer.length] = read.value;
-				continue;
-			}
-			if (len < 8)
-			{
-				//console.log('keyload...')
-				let i = 0;
-				while (i < read.value.length)
+				read = await reader.read();
+				if (read.done)
 				{
-					key[len++] = read.value[i++];
-					//console.log('keyload-' + i)
-					if (len > 7)
-					{
-						//console.log('keyloaded over')
-						break;
-					}
+					break;
 				}
 				if (len < 8)
 				{
-					//console.log('keyload contiune')
-					continue;
+					//console.log('keyload...')
+					let i = 0;
+					while (i < read.value.length)
+					{
+						key[len++] = read.value[i++];
+						//console.log('keyload-' + i)
+						if (len > 7)
+						{
+							//console.log('keyloaded over')
+							break;
+						}
+					}
+					if (len < 8)
+					{
+						//console.log('keyload contiune')
+						continue;
+					}
+					//console.log('keyloaded finish')
+					read.value = read.value.slice(i);
 				}
-				//console.log('keyloaded finish')
-				read.value = read.value.slice(i);
+				//console.log('payload...')
+				for (let i = 0; i < read.value.length; ++i)
+				{
+					read.value[i] = read.value[i] ^ key[offset++ % 8];
+				}
+				buffer[buffer.length] = read.value;
 			}
-			//console.log('payload...')
-			for (let i = 0; i < read.value.length; ++i)
-			{
-				read.value[i] = read.value[i] ^ key[offset++ % 8];
-			}
-			buffer[buffer.length] = read.value;
+			blob = new Blob(buffer, {type});
 		}
-		//console.log('payload finish')
-		const blob = new Blob(buffer, {type});
-		switch (type)
+		else
+		{
+			blob = await response.blob();
+		}
+		
+		switch (option.type || response.headers.get('content-type'))
 		{
 			case 'application/json': return JSON.parse(await blob.text());
 			case 'text/plain': return blob.text();
@@ -102,7 +100,6 @@ else
 		load(data.url, data.type, data.buffer ? {
 			...data.options,
 			method: 'POST',
-			headers: {'content-type': 'application/json'},
 			body: data.buffer
 		} : data.options)
 			.then(content => self.postMessage({id: data.id, is: 0, content}))
@@ -118,6 +115,7 @@ else
 				
 			});
 	}
+
 	
 	self.onmessage = event =>
 	{
