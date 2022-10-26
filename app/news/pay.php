179f5bb45eca5a258d2334e666f251e66b93a217
codedk47 <zeroneta47@icloud.com>
2022-10-26 14:58:01 +0700

    1

--- app/news/pay.php
+++ app/news/pay.php
@@ -842,12 +842,12 @@ final class webapp_router_pay extends webapp_echo_xml
 	}
 	function notify(string $name)
 	{
-		//file_put_contents('d:/n.txt', json_encode($result, JSON_UNESCAPED_UNICODE));
 		$result = match ($name)
 		{
 			'ny' => $this->webapp->request_content('application/json'),
 			default => $this->webapp->request_content(),
 		};
+		//file_put_contents('d:/n.txt', json_encode($result, JSON_UNESCAPED_UNICODE));
 		if (class_exists($channel = "webapp_pay_{$name}", FALSE)
 			&& (new $channel($this->webapp['app_pay'][$name]))->notify($result, $status)
 			&& $this->webapp->mysql->orders('WHERE hash=?s LIMIT 1', $status['hash'])->fetch($order)
