7762e53e96bfd1eccea26036637486a090cf0e16
codedk47 <zeroneta47@icloud.com>
2022-11-07 13:03:20 +0700

    1

--- app/news/admin.php
+++ app/news/admin.php
@@ -839,8 +839,9 @@ JS);
 			$table->cell($acc['did']);
 			$table->cell($acc['phone']);
 			$table->cell($acc['name']);
+			$table->cell($acc['history'] / 12);
 		});
-		$table->fieldset('账号(uid)', '注册日期(date)', '会员过期(expire)', '余额(balance)', '最后登录时间(lasttime)', '最后登录IP(lastip)', '设备类型(device)', '单位(unit)', '邀请账号(code)', '设备ID(did)', '绑定手机(phone)', '名称(name)');
+		$table->fieldset('账号(uid)', '注册日期(date)', '会员过期(expire)', '余额(balance)', '最后登录时间(lasttime)', '最后登录IP(lastip)', '设备类型(device)', '单位(unit)', '邀请账号(code)', '设备ID(did)', '绑定手机(phone)', '名称(name)', '记录(history)');
 		$table->header('Found %s item', number_format($table->count()));
 		$table->bar->select(['' => '全部单位', '0000' => '内部单位'] + $this->webapp->mysql->unitsets->column('name', 'unit'))
 			->setattr(['onchange' => 'g({unit:this.value?this.value:null})'])->selected($uint);
