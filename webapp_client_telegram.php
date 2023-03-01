b34845c50ae9e1325a4f9767eb8b0a3560d24f4e
codedk47 <zeroneta47@icloud.com>
2023-03-01 10:56:18 +0700

    1

---
+++ webapp_client_telegram.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+/*
+1.登录 Telegram 并转到 https://telegram.me/botfather
+2.点击网页界面中的 Start 按钮或输入 /start
+3.点击或输入 /newbot 并输入名称
+4.为聊天机器人输入一个用户名，该名称应以“bot”结尾（例如 garthsweatherbot）
+5.复制生成的访问令牌
+https://api.telegram.org/bot{token}/{method}
+https://core.telegram.org/bots/api#available-methods
+*/
+class webapp_client_telegram extends webapp_client_http
+{
+	function __construct(string $token)
+	{
+		parent::__construct("https://api.telegram.org/bot{$token}/");
+		//$this->debug();
+	}
+	function api(string $method, ?array $data = NULL):bool|array
+	{
+		$this->goto("{$this->url}{$method}", $data ? ['method' => 'POST', 'data' => $data] : []);
+		return $this->status($response) === 200
+			&& is_array($content = $this->content())
+			&& ($content['ok'] ?? FALSE) ? $content['result'] : FALSE;
+	}
+	static function message(array $data, ?webapp_client_telegram $telegram = NULL)
+	{
+		return new class($data, $telegram) extends ArrayObject
+		{
+			public readonly int $update_id;
+			function __construct(array $data, public readonly webapp_client_telegram $telegram)
+			{
+				parent::__construct($data['message'], ArrayObject::STD_PROP_LIST);
+				$this->update_id = $data['update_id'];
+			}
+			function __toString()
+			{
+				return $this['text'];
+			}
+			function reply_message(string $text, bool $private = FALSE)
+			{
+				$this->telegram->send_message($this[$private ? 'from' : 'chat']['id'], $text);
+			}
+		};
+	}
+	#https://core.telegram.org/bots/api#getupdates
+	function get_updates(int $offset = -1):array
+	{
+		return $this->api('getUpdates', $offset ? ['offset' => $offset] : NULL);
+	}
+	#https://core.telegram.org/bots/api#setwebhook
+	function set_webhook(string $url = NULL):bool
+	{
+		return $this->api('setWebhook', is_string($url) ? ['url' => $url] : NULL);
+	}
+		#https://api.telegram.org/bot{token}/getWebhookInfo
+		function get_webhook_info():array
+		{
+			return $this->api('getWebhookInfo');
+		}
+	function delete_message(int|string $chat_id, int $message_id):bool
+	{
+		return $this->api('deleteMessage', ['chat_id' => $chat_id, 'message_id' => $message_id]);
+	}
+	#https://core.telegram.org/bots/api
+	function get_me():array
+	{
+		return $this->api('getMe');
+	}
+	#https://core.telegram.org/bots/api#sendmessage
+	function send_message(int|string $chat_id, string $text):array
+	{
+		return $this->api('sendMessage', ['chat_id' => $chat_id, 'text' => $text]);
+	}
+}
