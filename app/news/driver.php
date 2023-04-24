20af1fadb6937777d016260632ea17caea62774f
codedk47 <zeroneta47@icloud.com>
2023-04-24 15:26:41 +0700

    1

--- app/news/driver.php
+++ app/news/driver.php
@@ -173,7 +173,8 @@ class news_driver extends webapp
 	}
 	function request_cid():string
 	{
-		return preg_match('/; CID\/(\w{4})/', $this->request_device, $cid) ? $cid[1] : '0000';
+		return is_string($cid = $this->request_cookie('CID')) && preg_match('/^\w{4}$/', $cid)
+			? $cid : (preg_match('/; CID\/(\w{4})/', $this->request_device, $cid) ? $cid[1] : '0000');
 	}
 	function request_did():?string
 	{
