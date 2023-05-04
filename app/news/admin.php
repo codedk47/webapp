5ee51da001716f6942b05f81b2a410b12b42e376
codedk47 <zeroneta47@icloud.com>
2023-05-04 17:17:24 +0700

    1

--- app/news/admin.php
+++ app/news/admin.php
@@ -632,7 +632,7 @@ class webapp_router_admin extends webapp_echo_html
 	}
 	function post_tag_create()
 	{
-		if (($this->webapp->admin[2] || $this->webapp->admin[0] == '1200') //<--------------
+		if (($this->webapp->admin[2] || $this->webapp->admin[0] == 1200) //<--------------
 			&& $this->form_tag($this->webapp)->fetch($tag)
 			&& $this->check_tags($tag['alias'])
 			&& ($this->webapp->admin[0] == '1200' ? boolval($tag['level'] = 9) : TRUE)
