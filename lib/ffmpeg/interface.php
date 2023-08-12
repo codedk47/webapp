96ce1ba43d3b5b696e4edf9ceefc87b0dc6d838d
codedk47 <zeroneta47@icloud.com>
2023-08-12 14:41:49 +0700

    1

--- lib/ffmpeg/interface.php
+++ lib/ffmpeg/interface.php
@@ -1,6 +1,6 @@
 <?php
 include 'ffmpeg.php';
-return function(string $filename, string $option = '-hide_banner -loglevel error -stats -y -hwaccel dxva2 -threads 4')
+return function(string $filename, string $option = '-hide_banner -loglevel error -stats -y -threads 4')
 {
 	return new ffmpeg($filename, $option);
 };
