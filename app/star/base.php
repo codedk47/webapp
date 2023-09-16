a36560531f26e3e40fa6a172b69eeeef8ebe5f7d
codedk47 <zeroneta47@icloud.com>
2023-09-16 22:44:47 +0700

    1

--- app/star/base.php
+++ app/star/base.php
@@ -404,9 +404,10 @@ class base extends webapp
 	}
 	function cid():string
 	{
-		return is_string($cid = $this->request_cookie('cid') ?? $this->query['cid'] ?? NULL)
-			&& preg_match('/^\w{4,}/', $cid) ? substr($cid, 0, 4)
-			: (preg_match('/CID\:(\w{4})/', $this->ua, $pattern) ? $pattern[1] : '0000');
+		return preg_match('/CID\:(\w{4})/', $this->ua, $pattern) ? $pattern[1] : '0000';
+		// return is_string($cid = $this->request_cookie('cid') ?? $this->query['cid'] ?? NULL)
+		// 	&& preg_match('/^\w{4,}/', $cid) ? substr($cid, 0, 4)
+		// 	: (preg_match('/CID\:(\w{4})/', $this->ua, $pattern) ? $pattern[1] : '0000');
 	}
 	function did():?string
 	{
