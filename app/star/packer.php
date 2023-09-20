d52a944f2863a08e4f1f70229de4c5d5d8929602
codedk47 <zeroneta47@icloud.com>
2023-09-20 19:08:43 +0700

    1

--- app/star/packer.php
+++ app/star/packer.php
@@ -170,8 +170,8 @@ class webapp_router_packer
 			// 	? ['iOS 下载', 'href' => $dl, 'class' => 'iphone', 'onclick' => 'return iphone(this)']
 			// 	: ['Android 下载', 'href' => $dl, 'class' => 'android']);
 			$html->xml->body->div[0]->main->a->setattr($this->type === 'iphone'
-				? ['iOS 下载', 'href' => '/pwa/uni.apk', 'class' => 'iphone', 'onclick' => 'return iphone(this)']
-				: ['Android 下载', 'href' => '/pwa/rstar.mobileconfig', 'class' => 'android']);
+				? ['iOS 下载', 'href' => '/pwa/rstar.mobileconfig', 'class' => 'iphone', 'onclick' => 'return iphone(this)']
+				: ['Android 下载', 'href' => '/pwa/uni.apk', 'class' => 'android']);
 		}
 		else
 		{
