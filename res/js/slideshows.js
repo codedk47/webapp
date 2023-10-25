4536311f8020e000fafeebed41ead397a8b6c740
codedk47 <zeroneta47@icloud.com>
2023-10-25 19:22:03 +0700

    1

--- res/js/slideshows.js
+++ res/js/slideshows.js
@@ -3,27 +3,23 @@ webapp-slideshows{
 	display: block;
 	position: relative;
 	overflow: hidden;
-
-	min-height: 12rem;
+	color: transparent;
 }
 webapp-slideshows>div:first-child{
 	width: 300%;
-	position: absolute;
-	top:0; bottom:0;
-	overflow: hidden;
 	display: flex;
+	position: absolute;
+	top: 0;
+	bottom: 0;
 }
 webapp-slideshows>div:first-child>a{
 	width: 100%;
 	display: block;
-	
-}
-
-webapp-slideshows>div:first-child>a:nth-child(1){background:red;}
-webapp-slideshows>div:first-child>a:nth-child(2){background:green;}
-webapp-slideshows>div:first-child>a:nth-child(3){background:blue;}
-`;
-
+	background: linear-gradient(to bottom right, silver, transparent);
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+}`;
 customElements.define('webapp-slideshows', class extends HTMLElement
 {
 	#slide = document.createElement('div');
@@ -33,20 +29,24 @@ customElements.define('webapp-slideshows', class extends HTMLElement
 		document.createElement('a')
 	];
 	#contents = [];
-
 	#slider;
-	#duration;
-	#index;
-
-	#onclick = event => event.isTrusted;
+	#index = 0;
 	constructor()
 	{
 		super();
 		this.#slide.style.left = '-100%';
-		this.#slide.append(...this.#slides.map(anchor => (anchor.onclick = event => this.#onclick(event), anchor)));
-
-
-		let transitioning = false, currentindex = 0, beforeindex = 0;
+		this.#slide.append(...this.#slides.map(anchor =>
+		{
+			anchor.onclick = event =>
+			{
+				event.stopPropagation();
+				return this.#index < this.#contents.length && this.onclick
+					? this.onclick.call(this.#contents[this.#index], event)
+					: event.isTrusted;
+			};
+			return anchor;
+		}));
+		let transitioning = false;
 		const position = {
 			offset: 0,
 			start: 0,
@@ -63,7 +63,6 @@ customElements.define('webapp-slideshows', class extends HTMLElement
 		this.#slide.addEventListener('touchmove', event =>
 		{
 			if (transitioning) return;
-			event.preventDefault();
 			position.move = event.clientX || event.touches[0].clientX;
 			this.#slide.style.left = `${position.offset + position.move - position.start}px`;
 		});
@@ -71,7 +70,7 @@ customElements.define('webapp-slideshows', class extends HTMLElement
 		{
 			if (transitioning) return;
 			let [left, direction, clicked] = typeof offset === 'boolean'
-				? [this.#slide.style.left, offset ? -101 : 101, false]
+				? [this.#slide.style.left, offset ? -101 : 101, this.#slide.style.left !== '-100%']
 				: [position.left, position.start - position.move, this.#slide.style.left === position.left];
 			if (clicked)
 			{
@@ -79,25 +78,22 @@ customElements.define('webapp-slideshows', class extends HTMLElement
 			}
 			if (this.#contents.length && Math.abs(direction) > 100)
 			{
-				beforeindex = currentindex;
-
 				if (direction < 0)
 				{
-					if (--currentindex < 0)
+					if (--this.#index < 0)
 					{
-						currentindex = this.#contents.length - 1;
+						this.#index = this.#contents.length - 1;
 					}
 					left = '0px';
 				}
 				else
 				{
-					if (++currentindex >= this.#contents.length)
+					if (++this.#index >= this.#contents.length)
 					{
-						currentindex = 0;
+						this.#index = 0;
 					}
 					left = '-200%';
 				}
-
 			}
 			if (this.#slide.style.left !== left)
 			{
@@ -112,19 +108,20 @@ customElements.define('webapp-slideshows', class extends HTMLElement
 			this.#slide.style.transition = null;
 			if (this.#slide.style.left !== '-100%')
 			{
-				console.log(this.#slide.style.left)
-				this.#slide.style.left = '-100%';
 				this.#setcontent(...this.#slide.style.left === '0px'
 					? [this.#slide.insertBefore(this.#slide.lastElementChild, this.#slide.firstElementChild),
-						this.#contents[currentindex - 1 < 0 ? this.#contents.length - 1 : currentindex - 1]]
+						this.#contents[this.#index - 1 < 0 ? this.#contents.length - 1 : this.#index - 1]]
 					: [this.#slide.appendChild(this.#slide.firstElementChild),
-						this.#contents[currentindex + 1 >= this.#contents.length ? 0 : currentindex + 1]]);
-				
+						this.#contents[this.#index + 1 >= this.#contents.length ? 0 : this.#index + 1]]);
+				this.#slide.style.left = '-100%';
 			}
 			transitioning = false;
 		});
 	}
-
+	slide(upordown)
+	{
+		this.#slider(Boolean(upordown));
+	}
 	#setcontent(node, content)
 	{
 		node.style.backgroundImage = `url(${content.picture})`;
@@ -142,16 +139,16 @@ customElements.define('webapp-slideshows', class extends HTMLElement
 	}
 	connectedCallback()
 	{
-		this.#duration = parseInt(this.dataset.duration, 10) || 4;
 		this.appendChild(this.#slide);
 		requestAnimationFrame(() =>
 		{
 			const contents = JSON.parse(this.lastChild.nodeValue);
 			Array.isArray(contents) && contents.forEach(content => this.addition(content));
-			// setInterval(() => {
-				
-			// }, parseInt(this.dataset.duration, 10) || 4);
-		
+			setInterval(this.#slider, this.dataset.duration * 1000 || 4000, this.hasAttribute('reverse'));
 		});
 	}
 });
+function cc(event)
+{
+	console.log(this, event)
+}
