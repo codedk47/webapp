1d7538b134c28a5c30c61c24dfb1bcb56eeb44f0
codedk47 <zeroneta47@icloud.com>
2023-10-30 15:52:42 +0700

    1

--- res/js/masker.js
+++ res/js/masker.js
@@ -178,7 +178,7 @@ else
 					return resources.then(() => request(`${origin}${url.search.substring(1)}`, true));
 				}
 				console.log(event)
-				if (event.isReload)
+				if (event.isReload || event.request.mode === 'navigate')
 				{
 					return new Response(new Blob(['<html lang="en"><head><meta charset="utf-8">',
 						`<script src="${self.location.href}" data-reload="${event.request.url}"></script>`,
