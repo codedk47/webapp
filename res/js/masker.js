ff7bcaee71997f731b2a7d94b4c9f4b077d97dcc
codedk47 <zeroneta47@icloud.com>
2023-11-05 14:33:47 +0700

    1

--- res/js/masker.js
+++ res/js/masker.js
@@ -196,23 +196,14 @@ else
 	self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 	// Claim any clients immediately, so that the page will be under SW control without reloading.
 	//self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-	self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-	// self.addEventListener('activate', function(event) {
-	// 	event.waitUntil(
-	// 	  caches.keys().then(function(cacheNames) {
-	// 		console.log(cacheNames)
-	// 		return Promise.all(
-	// 		  cacheNames.filter(function(cacheName) {
-	// 			// Return true if you want to remove this cache,
-	// 			// but remember that caches are shared across
-	// 			// the whole origin
-	// 		  }).map(function(cacheName) {
-	// 			return caches.delete(cacheName);
-	// 		  })
-	// 		);
-	// 	  })
-	// 	);
-	//   });
+	//self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+	self.addEventListener('activate', event => {
+
+		event.waitUntil(
+			caches.keys().then( keyList => {
+				return Promise.all(keyList.map( key => caches.delete(key)));
+			}).then(self.clients.claim())); //this line is important in some contexts
+	});
 	self.addEventListener('message', event =>
 	{
 		const promise = pending.get(event.data.pid);
