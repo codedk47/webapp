b10dfc93f8b4036dc24089cc341027a0fa4d01e1
codedk47 <zeroneta47@icloud.com>
2023-11-05 14:54:59 +0700

    1

--- res/js/masker.js
+++ res/js/masker.js
@@ -193,9 +193,9 @@ else
 	}));
 	// Skip the 'waiting' lifecycle phase, to go directly from 'installed' to 'activated', even if
 	// there are still previous incarnations of this service worker registration active.
-	addEventListener('install', event => skipWaiting());
+	addEventListener('install', event => event.waitUntil(skipWaiting()));
 	// Claim any clients immediately, so that the page will be under SW control without reloading.
-	addEventListener('activate', event => clients.claim());
+	addEventListener('activate', event => event.waitUntil(clients.claim()));
 	//addEventListener('activate', event => event.waitUntil(clients.claim()));
 
 	addEventListener('message', event =>
