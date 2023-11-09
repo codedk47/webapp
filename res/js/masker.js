f446ac2e8d947ebd2c048db23b4e8086b07dbbfb
codedk47 <zeroneta47@icloud.com>
2023-11-09 19:43:56 +0700

    1

--- res/js/masker.js
+++ res/js/masker.js
@@ -88,8 +88,8 @@ if (self.window)
 		frame.style.cssText = [
 			'position: fixed',
 			'inset: 0',
-			'width: 100%',
-			'height: 100%',
+			'width: 100vw',
+			'height: 100vh',
 			'border: none'
 		].join(';');
 		document.body.appendChild(frame).contentWindow.addEventListener('message', event =>
