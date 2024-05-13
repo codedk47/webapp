579b5264947c4cd30632a1f320419d0bcdd24a81
Your Name <you@example.com>
2024-05-13 15:51:13 +0700

    1

--- app/star/news.js
+++ app/star/news.js
@@ -2,14 +2,18 @@ addEventListener('DOMContentLoaded', event =>
 {
 	const video = document.createElement('video');
 	video.setAttribute('playsinline', 'true');
+	video.setAttribute('disablepictureinpicture', 'true');
+	video.setAttribute('muted', 'true');
+	video.autoplay = true;
+
 	document.querySelectorAll('div.videos>a>figure,div.playleft>a>figure,div.playright>a>figure').forEach(element =>
 	{
 		element.onmouseleave = event => element.removeChild(video);
 		element.onmouseenter = element.parentNode.ontouchstart = event =>
 		{
 			video.poster = element.firstElementChild.src;
-			video.muted = true;
-			video.autoplay = true;
+			
+			
 			video.src = element.dataset.preview;
 			element.appendChild(video);
 		};
