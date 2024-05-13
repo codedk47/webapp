085ff6837a7b94865befdca8e09c2e19576100b4
Your Name <you@example.com>
2024-05-13 15:48:23 +0700

    1

--- app/star/news.js
+++ app/star/news.js
@@ -5,7 +5,7 @@ addEventListener('DOMContentLoaded', event =>
 	document.querySelectorAll('div.videos>a>figure,div.playleft>a>figure,div.playright>a>figure').forEach(element =>
 	{
 		element.onmouseleave = event => element.removeChild(video);
-		element.onmouseenter = element.ontouchstart = event =>
+		element.onmouseenter = element.parentNode.ontouchstart = event =>
 		{
 			video.poster = element.firstElementChild.src;
 			video.muted = true;
