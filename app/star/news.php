4173516b7a601e693f71f0aecfd2772725a39915
Your Name <you@example.com>
2024-05-17 17:07:48 +0700

    1

--- app/star/news.php
+++ app/star/news.php
@@ -95,7 +95,7 @@ class webapp_router_news extends webapp_echo_html
 	}
 
 	
-	function get_home()
+	function get_home(int $page = 1)
 	{
 		$this->add_meta_seo($this->webapp['app_name'], $this->webapp['app_name']);
 		$this->set_header_nav();
@@ -103,7 +103,7 @@ class webapp_router_news extends webapp_echo_html
 		//$this->main->append('h1', 'test');
 
 
-		$this->add_div_videos($this->main, $this->webapp->fetch_videos->with('FIND_IN_SET("lqe2",tags)')->paging(1, 30));
+		$this->add_div_videos($this->main, $this->webapp->fetch_videos, $page);
 	}
 	function get_news()
 	{
