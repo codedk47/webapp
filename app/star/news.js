60206d52324bf6be02cfe2da68f6616edd12ce77
Your Name <you@example.com>
2024-05-20 16:16:50 +0700

    1

--- app/star/news.js
+++ app/star/news.js
@@ -6,20 +6,25 @@ addEventListener('DOMContentLoaded', event =>
 	video.setAttribute('muted', 'true');
 	video.autoplay = true;
 	video.loop = true;
-
+	let before;
 
 	document.ontouchstart = event =>
 	{
 		for (let p = event.target; p !== document; p = p.parentNode)
 		{
-			if (p.tagName === 'A' && p.dataset.preview)
+			if (before !== p && p.tagName === 'A' && p.dataset.preview)
 			{
+				before = p;
 				video.src = p.dataset.preview;
 				p.firstElementChild.appendChild(video);
+				break;
 			}
 		}
 	};
-
+	document.querySelectorAll('div.videos>a,div.playleft>a,div.playright>a').forEach(element =>
+	{
+		element.onmouseenter = event => document.ontouchstart(event);
+	});
 
 	return;
 	
