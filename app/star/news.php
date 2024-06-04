b7ef25b090a837955fe5cf2877e391aaeb199093
Your Name <you@example.com>
2024-06-04 16:32:27 +0700

    1

--- app/star/news.php
+++ app/star/news.php
@@ -225,9 +225,10 @@ By clicking ENTER below, you certify that you are 18 years or older.']);
 	{
 		$this->set_header_nav();
 		$tags = array_map(strtolower(...), $this->webapp->fetch_tags->shortname(LOCALE));
-		$keyword = join('%', array_filter(array_map(trim(...), explode(' ', strtolower(urldecode($keywords))))));
+		$keyword = join(' ', array_filter(array_map(trim(...), explode(' ', strtolower(urldecode($keywords))))));
+		var_dump($keyword);
 		$this->add_meta_seo($keyword, $this->webapp['app_name']);
-		$conditions = ['name LIKE ?s', "%{$keyword}%"];
+		$conditions = ['name LIKE ?s', sprint('"%s"', strtr($keyword, ' ', '%'))];
 		if ($tag = array_search($keyword, $tags))
 		{
 			$conditions[0] .= ' OR FIND_IN_SET(?s,tags)';
