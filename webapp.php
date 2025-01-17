c25ef16ec678d252bc188eada7e21a2060f1cb0f
ZERONETA <zeroneta47@icloud.com>
2025-01-17 23:48:07 +0800

    1

--- webapp.php
+++ webapp.php
@@ -350,7 +350,6 @@ abstract class webapp extends stdClass implements ArrayAccess, Stringable, Count
 	}
 	static function maskdata(string $source):string
 	{
-		
 		$bin = static::random(8);
 		$key = array_map(ord(...), str_split($bin));
 		$length = strlen($source);
